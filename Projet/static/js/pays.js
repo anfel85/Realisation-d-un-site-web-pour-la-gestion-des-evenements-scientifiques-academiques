a8974999c384d2d691fc1cbc4f4939793ccb68ba
dRAOUF1 <119814672+dRAOUF1@users.noreply.github.com>
2024-06-03 23:51:24 +0100

    raouf

--- Projet/static/js/pays.js
+++ Projet/static/js/pays.js
@@ -1,10 +1,10 @@
 console.log("Le script JavaScript est en cours d'exécution !");
-
 async function fetchCountries() {
   const response = await fetch("https://restcountries.com/v3.1/all");
   const data = await response.json();
   return data;
 }
+console.log("Le script JavaScript est en cours d'exécution !");
 
 async function populateCountries() {
   const countries = await fetchCountries();
@@ -25,7 +25,7 @@ async function populateCountries() {
   });
 
   selectElement.addEventListener("change", (event) => {
-    hiddenInput.value = event.target.value; // Met à jour la valeur de l'élément input caché
+    hiddenInput.value = event.target.value;
   });
 }
 
